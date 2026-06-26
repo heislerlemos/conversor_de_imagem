0ee79e972c31e107cfe85c085e824e2b009349c6
heislerlemos <heisler.stlano@gmail.com>
2026-06-26 11:08:30 +0100

    arrainging text

--- main.rb
+++ main.rb
@@ -16,29 +16,35 @@ window('Programa de conversão de visto de angola', 900 , 400) {
         square(0, 0, 890)
         fill r: 82, g: 32, b: 129, a: 1.0
       }
-      text {
+
+      path { 
+        rectangle(0, 100, 200, 200)
+        rectangle(100, 0, 650, 150)
+        fill x0: 10, y0: 10, x1: 350, y1: 350, stops: [{pos: 0.25, r: 204, g: 102, b: 204}, {pos: 0.75, r: 102, g: 102, b: 204}]
+      }
+
+      text(100, 100, 700) {
         default_font family: 'Helvetica', size: 12, weight: :normal, italic: :normal, stretch: :normal
         string("Introduza imagem para elaborar conversão das imagens e modificar o tamanho para que seja aceita no consulado de angola") {
-          font size: 20, weight: :bold, italic: :normal, stretch: :normal
+          font family: 'Ubuntu', size: 20, weight: :bold, italic: :normal, stretch: :normal
           color r: 255, g: 255, b: 255, a: 1
         } 
       }
     }
 
     button('Verificar requisitos do visto de angola') do
-        stretchy false
+      stretchy false
       on_clicked do
         msg_box('Requisitos', ficheiro_requisito)
       end
     end
 
     form {
-       stretchy false
+      stretchy false
 
       @caminho = entry { 
-        label 'insira o caminho da imagem'
+        label 'insira o caminho da imagem do passport'
       }
-
     }
 
     button('Converter imagem') {
